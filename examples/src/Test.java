70edb3d96e9e097fe5896ac927d050434302231e
apauld <aaron.dix@gmail.com>
2013-01-24 19:27:59 -0700

    Adding Test class

---
+++ examples/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String hello = "Hello";
+		int x = 100;
+		x = 200;
+		System.out.println(x);
+	}
+	
+
+}
